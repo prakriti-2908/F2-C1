77e1b7f9745771593cfc7068e022115baac74404
prakriti-2908 <77171197+prakriti-2908@users.noreply.github.com>
2024-02-17 13:38:19 +0530

    Update script.js finalSolution

--- script.js
+++ script.js
@@ -6,22 +6,55 @@ let arr = [
   { id: 3, name: "karen", age: "19", marks: 35 },
 ];
 
-function PrintStudentswithMap() {
+function PrintStudentswithMap(arr) {
   //Write your code here , just console.log
+  arr.map(student => {
+        if (student.marks > 50) {
+            console.log(student.name);
+        }
+    });
 }
+PrintStudentswithMap(arr);
 
-function PrintStudentsbyForEach() {
+function PrintStudentsbyForEach(arr) {
   //Write your code here , just console.log
+  arr.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student.name);
+    }
+  });
 }
+PrintStudentsbyForEach(arr);
 
-function addData() {
-  //Write your code here, just console.log
-}
 
-function removeFailedStudent() {
+function addData(arr) {
   //Write your code here, just console.log
+ let newObj = {id:4,name:"susan",age:"20",marks:45};
+ arr.push(newObj);
+ console.log(arr);
 }
+addData(arr);
 
-function concatenateArray() {
+function removeFailedStudent(arr) {
   //Write your code here, just console.log
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i].marks < 50) {
+      arr.splice(i, 1);
+    }
+  }
+  console.log(arr);
 }
+removeFailedStudent(arr);
+
+function concatenateArray(arr) {
+  //Write your code here, just console.log
+  
+let arr2 = [
+    ...arr,
+  { id: 4, name: "priya", age: "12", marks: 90 },
+  { id: 5, name: "malvi", age: "25", marks: 13 },
+  { id: 6, name: "sushmita", age: "20", marks: 99 },
+];
+console.log(arr2);
+}
+concatenateArray(arr);
